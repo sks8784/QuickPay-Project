d52024295d34c923c630f74fd64abc7027e63bbc
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2024-06-30 18:23:12 +0530

    changes

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
     // TODO: Check if this onRampTxn is processing or not
     
